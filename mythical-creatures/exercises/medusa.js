3652d39f299584b0c6e7c7b8bcd9b14312684698
Karrar Qasim <Karawr1996@gmail.com>
2022-10-25 09:38:48 -0600

    Mythical creatures, medusa

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,35 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+    constructor(name) {
+        this.name = name;
+        this.statues = [];
+    }
+  
+    gazeAtVictim(victims) {
+        var newstatue = new Statue(victims.name)  
+        if(this.statues.length <= 2) {
+            this.statues.push(newstatue)
+        } else if(this.statues.length >= 3) {
+            var newPerson = new Person(this.statues[0].name)
+            newPerson.mood = 'relieved'
+            this.statues.shift()
+            this.statues.push(newstatue)
+            return newPerson
+        }     
+    }
+}
+
+module.exports = Medusa;
+
+// this.statues.push(newstatue)
+
+// if(this.statues.length === 4) {
+
+//     var freePerson = this.statues.shift()
+//     var newPerson = new Person(freePerson.name)
+//     newPerson.mood = 'relieved'
+//     return newPerson
+
+// }
